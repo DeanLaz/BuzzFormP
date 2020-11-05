44840d8b90c3621538fb8bd470b1e6b33e77883e
DeanLaz <DeanLazx@gmail.com>
2020-11-04 16:44:22 -0800

    BuzzForm NPS Package in Production Sample

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import XForm from "buzzform";
+function App() {
+  return (
+    <div>
+      <XForm />
+    </div>
+  );
+}
+
+export default App;
